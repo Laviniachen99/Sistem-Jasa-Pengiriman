f96527a23ae1ba7ba096b2af95fe0f80b1c71874
laviniachen99 <laviniachen99@gmail.com>
2019-06-29 13:21:59 +0700

    Android Manifest Edit, Adding Maps with Zoom Tools and Current Location

--- app/src/main/java/com/example/qrcodeencode/MapsActivity.java
+++ app/src/main/java/com/example/qrcodeencode/MapsActivity.java
@@ -8,19 +8,38 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final int LOCATION_REQUEST = 500;
+    ArrayList<LatLng> listPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        listPoints = new ArrayList<>();
     }
 
     /**
@@ -51,7 +71,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //reset marker when already 2
+                if(listPoints.size() == 2){
+                    listPoints.clear();
+                    mMap.clear();
+                }
+                //save first point select
+                listPoints.add(latLng);
 
+                //create marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                if(listPoints.size()==1){
+                    //add first marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                }
+                else {
+                    //add second marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+                mMap.addMarker(markerOptions);
+
+                //TODO: request get direction code below
+                if(listPoints.size()==2 ){
+                    //Create the URL to get request from first marker to second marker
+                    String url = getRequestUrl(listPoints.get(0), listPoints.get(1));
+                    TaskRequestDirection taskRequestDirection = new TaskRequestDirection();
+                    taskRequestDirection.execute(url);
+                }
+            }
+        });
+
+    }
+
+    private String getRequestUrl(LatLng origin, LatLng destination) {
+        //Value of origin
+        String str_org = "origin = " + origin.latitude + "," +origin.longitude;
+        //Vakue of destination
+        String str_dest = "destination =" + destination.latitude + "," +destination.longitude;
+        //Set value enable to sensor
+        String sensor = "sensor = false";
+        //Mode for find direction
+        String mode = "mode=driving";
+        //Build the full parameters
+        String param = str_org+"&"+str_dest + "&" + sensor + "&" +mode;
+        //Output format
+        String output = "json";
+        //Create url to request
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + param;
+        return url;
+    }
+
+    private String requestDirection(String reqUrl){
+        String responseString = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try{
+            URL url = new URL(reqUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            //get the response result
+
+            inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while ((line = bufferedReader.readLine())!= null){
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            if (inputStream != null){
+                inputStream.close();
+            }
+            httpURLConnection.disconnect();
+        }
+        return responseString;
     }
 
     @SuppressLint("MissingPermission")
@@ -63,6 +171,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mMap.setMyLocationEnabled(true);
                 }
                 break;
+        }
+    }
+
+    public class TaskRequestDirection extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            String responseString ="";
+            try{
+                responseString = requestDirection(strings[0]);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return  responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Parse json here
+         TaskParser taskParser = new TaskParser();
+         taskParser.execute(s);
+        }
+    }
+    public class TaskParser extends AsyncTask <String, Void, List<List<HashMap<String, String>>>>{
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... strings) {
+            JSONObject jsonObject = null;
+            List<List<HashMap<String, String>>> routes = null;
+            try{
+                jsonObject = new JSONObject(strings[0]);
+                DirectionsParser directionsParser = new DirectionsParser();
+                routes = directionsParser.parse(jsonObject);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> lists) {
+            // Getlist route and display it into the maps
+            ArrayList points = null;
+
+            PolylineOptions polylineOptions = null;
+
+            for (List<HashMap<String, String>> path : lists){
+                points = new ArrayList();
+                polylineOptions = new PolylineOptions();
+
+                for (HashMap<String, String> point : path){
+                 double lat = Double.parseDouble(point.get("lat"));
+                 double lon = Double.parseDouble(point.get("lon"));
+
+                 points.add(new LatLng (lat, lon));
+
+                }
+
+                polylineOptions.addAll(points);
+                polylineOptions.width(15);
+                polylineOptions.color(Color.GREEN);
+                polylineOptions.geodesic(true);
+            }
+
+            if(polylineOptions!=null){
+                mMap.addPolyline(polylineOptions);
+            } else {
+                Toast.makeText(getApplicationContext(), "Direction not found!", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
